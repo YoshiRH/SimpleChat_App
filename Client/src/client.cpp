346b5829bb0d558d7dddb554d1973ccc5b983fb8
Yoshie <yoshuaaa7@gmail.com>
2025-02-17 16:59:36 +0100

    Set up client solution and implented connect to server function.

---
+++ Client/src/client.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 55555
+
+int main() {
+	WSADATA wsaData;
+	WORD version = MAKEWORD(2, 2);
+
+	// Find dll and initialize winsock
+	if (WSAStartup(version, &wsaData) != 0) {
+		std::cerr << "Couldn't initialize winsock...\n";
+		return 0;
+	}
+	else {
+		std::cout << "Winsock dll found!\n";
+		std::cout << "Status: " << wsaData.szSystemStatus << std::endl;
+	}
+
+	// Create client sockiet [IPv4, TCP]
+	SOCKET clientSocket = INVALID_SOCKET;
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (clientSocket == INVALID_SOCKET) {
+		std::cerr << "Couldn't create a client socket\n";
+		WSACleanup();
+		return 0;
+	}
+	else {
+		std::cout << "Client socket created!\n";
+	}
+
+	sockaddr_in clientAddr;
+	clientAddr.sin_family = AF_INET;
+	clientAddr.sin_port = htons(PORT);
+	inet_pton(AF_INET, SERVER_IP, &clientAddr.sin_addr.s_addr);
+
+	if (connect(clientSocket, (sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
+		std::cerr << "Couldn't connect to the server\n";
+		WSACleanup();
+		return 0;
+	}
+	else {
+		std::cout << "Connected!\n";
+	}
+
+	system("pause");
+	WSACleanup();
+
+
+	return 0;
+}
