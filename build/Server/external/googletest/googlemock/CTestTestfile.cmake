b587eb5c2fb8293449f794317be88d0c21172d1c
Yoshie <yoshuaaa7@gmail.com>
2025-03-10 17:36:40 +0100

    fix

--- build/Server/external/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Kodowanie/SimpleChat_App/Server/external/googletest/googlemock
-# Build directory: D:/Kodowanie/SimpleChat_App/build/Server/external/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
